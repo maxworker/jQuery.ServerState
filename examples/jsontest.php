8552ef6af39496e214344d5da8e55426171c3a6f
Maksim <mlbutenko@gmail.com>
2019-06-19 16:15:43 +0300

    Samples update

--- examples/jsontest.php
+++ examples/jsontest.php
@@ -3,5 +3,5 @@ $params = file_get_contents('php://input');
 $params = !empty($params) ? json_decode($params, true) : [];
 
 header('Content-Type: application/json');
-$message = array_key_exists("message", $params) ? $params["message"] : "Init Value";
+$message = array_key_exists("message", $params) ? $params["message"] : "Default Value";
 echo json_encode((object)["message"=>$message, "result"=>"Cool! You type: $message"]);
--- examples/simpletest.php
+++ examples/simpletest.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json');
-$message = array_key_exists("message", $_REQUEST) ? $_REQUEST["message"] : "";
+$message = array_key_exists("message", $_REQUEST) ? $_REQUEST["message"] : "Default!!!";
 echo json_encode((object)["message"=>$message, "result"=>"Cool! You type: $message"]);
                                                                            
